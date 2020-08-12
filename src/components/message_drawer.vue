8a4c245f5aefba12cd16288ccda3b614b7b50291
HeySearra <48943939+HeySearra@users.noreply.github.com>
2020-08-12 17:04:37 +0800

    放弃css转战js

--- src/components/message_drawer.vue
+++ src/components/message_drawer.vue
@@ -1,20 +1,25 @@
 <template>
 <el-container>
     <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">打开</el-button>
-<el-drawer
-  title="消息中心"
-  :visible.sync="drawer"
-  :with-header="true">
-    <message-item></message-item>
-    <message-item></message-item>
-    <message-item :is_read=true></message-item>
-    <message-item :is_read=true></message-item>
-     <!-- <el-table :data="gridData">
-      <el-table-column property="date" label="日期" width="150"></el-table-column>
-      <el-table-column property="name" label="姓名" width="200"></el-table-column>
-      <el-table-column property="address" label="地址"></el-table-column>
-    </el-table> -->
-</el-drawer>
+    <el-drawer
+    title="消息中心"
+    :visible.sync="drawer"
+    :with-header="false">
+        <div class="el-drawer__header">
+            <div class="el-drawer__title">消息中心</div>
+            <div class="mark-read">全部已读</div>
+            <!-- <div class="close-icon"><span v-if="type!='comment'" class="icon iconfont">&#xe79b;</span></div> -->
+        </div>
+        <message-item></message-item>
+        <message-item></message-item>
+        <message-item :is_read=true></message-item>
+        <message-item :is_read=true></message-item>
+        <!-- <el-table :data="gridData">
+        <el-table-column property="date" label="日期" width="150"></el-table-column>
+        <el-table-column property="name" label="姓名" width="200"></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+        </el-table> -->
+    </el-drawer>
 </el-container>
 </template>
 
@@ -50,11 +55,37 @@ export default {
 </script>
 
 <style scoped>
+    >>>.el-drawer__body{
+        width: 600px;
+        min-width: 300px !important;
+    }
     >>>.el-drawer{
-        width: 500px;
+        width: 600px;
         min-width: 300px !important;
     }
     >>>.el-drawer__header{
         border: 0;
+        padding: 0;
+        margin: 0;
+    }
+    >>>.el-drawer__title{
+        margin: 15px 15px;
+        border: 2px;
+        cursor: default;
+    }
+    /* .close-icon{
+        padding: 0 10px 0 0;
+        border: 2px;
+        vertical-align: top;
+    } */
+    .mark-read{
+        font-size: 90%;
+        border: 2px;
+        float: right;
+        margin: 0 15px;
+        cursor: pointer;
+    }
+    .mark-read:hover{
+        color: hsl(219, 15%, 23%);
     }
 </style>
--- src/components/message_item.vue
+++ src/components/message_item.vue
@@ -1,16 +1,20 @@
 <template>
     <div class="message-item" @click="mark_read">
-        <div class="profile">
-            <span v-if="type!='comment'" class="icon iconfont">&#xe622;</span>
-            <el-avatar v-if="type=='comment'" :src="img" size="big" style="vertical-align: middle;"></el-avatar>
+        <div class="item">
+            <div class="profile">
+                <span v-if="type!='comment'" class="icon iconfont">&#xe622;</span>
+                <el-avatar v-if="type=='comment'" :src="img" size="big" style="vertical-align: middle;"></el-avatar>
+            </div>
+            <div class="content">
+                <h4 class="title">
+                    <div class="content">{{content}}</div>
+                    <div class="message-head" v-if="type=='comment'" @click="to_doc">{{title}}</div>
+                    <div class="message-head" v-if="type=='join'" @click="confirm_join">{{team_name}}</div>
+                    <div class="message-head" v-if="type=='admin' || type=='remove'" @click="type=='admin'? 'to_team':''">{{team_name}}</div>
+                </h4>
+                <div class='comment'>{{comment}}</div>
+            </div>
         </div>
-        <h4 class="title">
-            <div class="content">{{content}}</div>
-            <div class="message-head" v-if="type=='comment'" @click="to_doc">{{title}}</div>
-            <div class="message-head" v-if="type=='join'" @click="confirm_join">{{team_name}}</div>
-            <div class="message-head" v-if="type=='admin' || type=='remove'" @click="type=='admin'? 'to_team':''">{{team_name}}</div>
-        </h4>
-        <div class='comment'>{{comment}}</div>
         <div class="not-read" v-if="!is_read"></div>
     </div>
 </template>
@@ -36,8 +40,8 @@ export default {
         return {
             content: '我是前缀', //前缀
             did: 0, //文档id
-            title: '我是文档标题', //文档标题
-            comment: '3213', //评论
+            title: '我是文档标题 我是文档标题我是文档标题我是文档标题我是文档标题', //文档标题
+            comment: '321凤凰发灰黑服啊和附件和佛教咖啡机能让肌肤发基坑俊娥既然你3', //评论
             tid: 0,
             team_name: '我是团队名',
             muid:0, //发表评论的uid
@@ -63,26 +67,39 @@ export default {
     position: relative;
     padding: 5px 0;
 }
+.item{
+    display:flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+}
 .profile{
-    float: left;
+    justify-content: flex-start;
     position: relative;
     padding: 10px;
+    width: 40px;
+    margin: 0 0 0 0;
 }
 .content{
-    display:inline-block;
-    *display:inline;
-    *zoom:1;
-    width:fit-content;
-    width:-webkit-fit-content;
-    width:-moz-fit-content;
+    display: flex;
+    flex-direction: column;
+    margin-right: 4px;
+    width:auto;
+    word-break:keep-all;
+    white-space:nowrap
 }
 .message-head{
     display:inline-block;
-    *display:inline;
-    *zoom:1;
+    width: auto;
+    max-width: 300px;
+    min-width: 180px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
 }
 .title{
-    display:inline-block;
+    display: inline-flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
     width:fit-content;
     width:-webkit-fit-content;
     width:-moz-fit-content;
@@ -99,5 +116,12 @@ export default {
 }
 .comment{
     font-size: 95%;
+    width: 100%;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    flex-grow:0;
+    flex-shrink:0;
+
 }
 </style>
