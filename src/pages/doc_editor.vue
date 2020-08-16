324a1f4bc5afa8c3cd89565dac7d727094508a5c
lucieniii <lucienli0320@gmail.com>
2020-08-16 23:19:36 +0800

    修复文章编辑

--- src/pages/doc_editor.vue
+++ src/pages/doc_editor.vue
@@ -36,17 +36,6 @@
 
 <script>
 import CKEditor from '@ckeditor/ckeditor5-build-decoupled-document';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
-
-import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 import '@ckeditor/ckeditor5-build-decoupled-document/build/translations/zh-cn';
 import {alert_box, alert_msg, console_debug} from "../assets/global";
 
@@ -79,39 +68,6 @@ const pageData = {
                     and the language a person speaks is an essential element of daily life.\
                 </p>'
 };
-
-class CreateTemplate extends Plugin {
-  init() {
-    const editor = this.editor;
-
-    editor.ui.componentFactory.add( 'insertImage', locale => {
-      const view = new ButtonView( locale );
-
-      view.set( {
-        label: 'Insert image',
-        icon: imageIcon,
-        tooltip: true
-      } );
-
-      // Callback executed once the image is clicked.
-      view.on( 'execute', () => {
-        const imageUrl = prompt( 'Image URL' );
-
-        editor.model.change( writer => {
-          const imageElement = writer.createElement( 'image', {
-            src: imageUrl
-          } );
-
-          // Insert the image in the current selection location.
-          editor.model.insertContent( imageElement, editor.model.document.selection );
-        } );
-      } );
-
-      return view;
-    } );
-  }
-}
-
 
 class MyUploadAdapter {
 
